f00b47dba474f07ac198b636407f229a777fe637
Valentin Khanaev <vkhanaev@ya.ru>
2024-06-08 08:06:09 +0300

    Добавлено отображение книг

--- books/views.py
+++ books/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.core import serializers
+from django.http import HttpResponse, HttpRequest, JsonResponse
+from django.shortcuts import render, get_object_or_404
+
+from books.models import Book
+
+
+def show_books(request: HttpRequest) -> HttpResponse:
+    books = Book.objects.all()
+    context = {"books": books}
+    return render(request, "books/books.html", context=context)
+
+
+def show_book(request: HttpRequest, book_id: int):
+    book = get_object_or_404(Book, pk=book_id)
+    context = {"book": book}
+    return render(request, "books/book-description.html", context=context)
+
+
+def convert_book_to_json(book: Book) -> json:
+    return {
+            "title": book.title,
+            "author_full_name": book.author_full_name,
+            "year_of_publishing": book.year_of_publishing,
+            "copies_printed": book.copies_printed,
+            "short_description": book.short_description,
+    }
+
+def show_books_json(request: HttpRequest):
+    books = Book.objects.all()
+    books = [
+        convert_book_to_json(book)
+        for book in books
+    ]
+    return JsonResponse({"books": books},  json_dumps_params={'ensure_ascii': False})
+
+
+def show_book_json(request: HttpRequest, book_id: int):
+    book = Book.objects.get(pk=book_id)
+    book = convert_book_to_json(book)
+    return JsonResponse(book, json_dumps_params={'ensure_ascii': False})
--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
+from books.views import show_books, show_book, show_books_json, show_book_json
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', show_books),
+    path('books/<int:book_id>/', show_book),
+    path('api/books/', show_books_json),
+    path('api/books/<int:book_id>/', show_book_json),
 ]
